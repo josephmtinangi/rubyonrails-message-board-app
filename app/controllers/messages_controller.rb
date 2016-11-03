7fcb827d45232f8a6d0c6e87ab483f4375b1532f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-03 18:05:28 +0300

    Save messages to database

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,11 @@ class MessagesController < ApplicationController
 
 	def create
 		@message = Message.new(message_params)
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	private
